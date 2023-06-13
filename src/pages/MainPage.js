cf57f9b06e59862832f74ad618b3939e6c4cdedb
mahdi mostajeran <mahdi.mostajeran@tapsi.cab>
2023-06-13 20:52:15 +0330

    movie api

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,57 +1,71 @@
-import { useState } from "react";
+import { useState, useEffect   } from "react";
 import { useNavigate } from "react-router-dom";
 import { Row, Col, Button} from "antd";
 import header from "../images/eastwood.jpg";
 import { StarTwoTone } from "@ant-design/icons";
 import { Link } from "react-router-dom";
 import NavBar from "../components/navBar";
+import axios from 'axios';
 
 const MainPage = () => {
   let navigate = useNavigate();
+
+  
   const [movies, setMovies] = useState([
-    {
-      id: 1,
-      name: "The Whale",
-      score: 7.8,
-      image:
-        "https://m.media-amazon.com/images/M/MV5BZDQ4Njg4YTctNGZkYi00NWU1LWI4OTYtNmNjOWMyMjI1NWYzXkEyXkFqcGdeQXVyMTA3MDk2NDg2._V1_.jpg",
-    },
-    {
-      id: 2,
-      name: "Seven",
-      score: 7.8,
-      image:
-        "https://m.media-amazon.com/images/M/MV5BOTUwODM5MTctZjczMi00OTk4LTg3NWUtNmVhMTAzNTNjYjcyXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_.jpg",
-    },
-    {
-      id: 3,
-      name: "Unforgiven",
-      score: 7.8,
-      image:
-        "https://m.media-amazon.com/images/M/MV5BODM3YWY4NmQtN2Y3Ni00OTg0LWFhZGQtZWE3ZWY4MTJlOWU4XkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_.jpg",
-    },
-    {
-      id: 4,
-      name: "Conjuring",
-      score: 7.8,
-      image:
-        "https://m.media-amazon.com/images/M/MV5BMTM3NjA1NDMyMV5BMl5BanBnXkFtZTcwMDQzNDMzOQ@@._V1_.jpg",
-    },
-    {
-      id: 5,
-      name: "The Shining",
-      score: 7.8,
-      image:
-        "https://m.media-amazon.com/images/M/MV5BZWFlYmY2MGEtZjVkYS00YzU4LTg0YjQtYzY1ZGE3NTA5NGQxXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_.jpg",
-    },
-    {
-      id: 6,
-      name: "Godfather",
-      score: 7.8,
-      image:
-        "https://m.media-amazon.com/images/M/MV5BM2MyNjYxNmUtYTAwNi00MTYxLWJmNWYtYzZlODY3ZTk3OTFlXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_.jpg",
-    },
+    // {
+    //   id: 1,
+    //   name: "The Whale",
+    //   score: 7.7,
+    //   image:
+    //     "https://m.media-amazon.com/images/M/MV5BZDQ4Njg4YTctNGZkYi00NWU1LWI4OTYtNmNjOWMyMjI1NWYzXkEyXkFqcGdeQXVyMTA3MDk2NDg2._V1_.jpg",
+    // },
+    // {
+    //   id: 2,
+    //   name: "Seven",
+    //   score: 8.6,
+    //   image:
+    //     "https://m.media-amazon.com/images/M/MV5BOTUwODM5MTctZjczMi00OTk4LTg3NWUtNmVhMTAzNTNjYjcyXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_.jpg",
+    // },
+    // {
+    //   id: 3,
+    //   name: "Unforgiven",
+    //   score: 8.2,
+    //   image:
+    //     "https://m.media-amazon.com/images/M/MV5BODM3YWY4NmQtN2Y3Ni00OTg0LWFhZGQtZWE3ZWY4MTJlOWU4XkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_.jpg",
+    // },
+    // {
+    //   id: 4,
+    //   name: "Conjuring",
+    //   score: 7.5,
+    //   image:
+    //     "https://m.media-amazon.com/images/M/MV5BMTM3NjA1NDMyMV5BMl5BanBnXkFtZTcwMDQzNDMzOQ@@._V1_.jpg",
+    // },
+    // {
+    //   id: 5,
+    //   name: "The Shining",
+    //   score: 8.4,
+    //   image:
+    //     "https://m.media-amazon.com/images/M/MV5BZWFlYmY2MGEtZjVkYS00YzU4LTg0YjQtYzY1ZGE3NTA5NGQxXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_.jpg",
+    // },
+    // {
+    //   id: 6,
+    //   name: "Godfather",
+    //   score: 9.2,
+    //   image:
+    //     "https://m.media-amazon.com/images/M/MV5BM2MyNjYxNmUtYTAwNi00MTYxLWJmNWYtYzZlODY3ZTk3OTFlXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_.jpg",
+    // },
   ]);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await axios.get('http://cinemashelf.ir/movie/');
+        console.log(response);
+        setMovies(response.data);
+      } catch (error) {
+      }
+    })();
+  }, []);
 
   const loginPage = () => {
     navigate("/login");
